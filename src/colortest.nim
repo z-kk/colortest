cf73b136d5f6a81babd16fb6a18f023469d6defa
z-kk <z-kk@gmail.com>
2023-02-01 22:24:51 +0900

    make main proc

--- src/colortest.nim
+++ src/colortest.nim
@@ -1,5 +1,26 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import
+  std / [strformat]
+
+proc echoColor(i: int) =
+  stdout.write "\e[38;5;{i}m {i:3}".fmt
 
 when isMainModule:
-  echo("Hello, World!")
+  for i in 0 ..< 2:
+    for j in 0 ..< 8:
+      echoColor(i * 8 + j)
+    echo ""
+  echo ""
+
+  for i in 0 ..< 12:
+    for j in 0 ..< 3:
+      for k in 0 ..< 6:
+        echoColor(16 + i * 6 + j * 6 * 6 * 2 + k)
+      stdout.write " "
+    echo ""
+    if (i + 1) mod 6 == 0:
+      echo ""
+
+  for i in 0 ..< 2:
+    for j in 0 ..< 12:
+      echoColor(16 + 6 * 6 * 6 + i * 12 + j)
+    echo ""
